0951d41537a5b2566daf6114a37dc6c250050929
suchithraspoojary <166589042+suchithraspoojary@users.noreply.github.com>
2024-04-10 12:10:41 +0530

    Add files via upload

---
+++ PRIME.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=0,b=1,c,n,count=0
+clrscr();
+printf("enter a value:\n");
+scanf("%d",&n);
+printf("first%d fibanoci number\n",n);
+printf("%d\t%d\t",a,b);
